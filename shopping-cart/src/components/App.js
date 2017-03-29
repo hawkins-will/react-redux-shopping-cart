c1f3355cf39a4afce901bc0cd603cb83dd9e7a24
Will Hawkins <willhawkins123@yahoo.com>
2017-03-29 15:36:29 -0400

    Followed Directions and Completed the Lab

---
+++ shopping-cart/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Cart from "../containers/Cart"
+import logo from '../logo.svg';
+import '../stylesheets/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Cart />
+    );
+  }
+}
+
+export default App;
